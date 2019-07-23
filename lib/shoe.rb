849d028d55f72551ea7bb44d3be1a39d050d4ef1
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-23 21:44:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,12 @@
-def 'Shoe' do
-  let(:shoe) { Shoe.new("Nike") }
+class Shoe 
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand 
+  
+  BRAND = []
+  
+  def initialize(brand)
+    @brand = brand
+    if !(BRAND.include?(@brand)
+      BRANDS << @brand 
+    end 
+  end 
