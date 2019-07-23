8c86a7f00af2c4926e57f6ef6a12622cef756fce
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-23 21:57:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-  BRANDS << @brand unless BRANDS.any? { |b| b == brand
+  BRANDS << @brand unless BRANDS.any? { |b| b == brand }
     end 
   end 
   
